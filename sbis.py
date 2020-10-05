2bbfe2fdac169aee745fff1c0bc07200f7eb0c1e
ia.belyaeva <ia.belyaeva@tensor.ru>
2020-10-05 10:13:51 +0300

    2-е видео с тренинга, установка виртуального окружения, первые тесты записанные рекордером

---
+++ sbis.py
@@ -0,0 +1,35 @@
+{
+  "id": "1ac9eded-9104-4569-ac83-58522d1768d5",
+  "version": "2.0",
+  "name": "Training",
+  "url": "https://fix.sbis.ru",
+  "tests": [{
+    "id": "94cc998f-57ad-47f7-bd54-0de1f3843190",
+    "name": "sbis",
+    "commands": [{
+      "id": "cf1595b4-f032-4c14-85d5-c3a81789c968",
+      "comment": "",
+      "command": "open",
+      "target": "/",
+      "targets": [],
+      "value": ""
+    }, {
+      "id": "17249215-4081-4a1b-9597-da0fdab753ba",
+      "comment": "",
+      "command": "setWindowSize",
+      "target": "1936x1056",
+      "targets": [],
+      "value": ""
+    }, {
+      "id": "91456a44-9ede-4807-a150-8a7a17f82e02",
+      "comment": "",
+      "command": "click",
+      "target": "css=.sbis_ru-menu__item-wrap:nth-child(9) .sbis_ru-menu__caption",
+      "targets": [
+        ["css=.sbis_ru-menu__item-wrap:nth-child(9) .sbis_ru-menu__caption", "css:finder"],
+        ["xpath=//div[@id='ws-jue5q1ssaq1601658760941']/ul/li[9]/a/span[2]", "xpath:idRelative"],
+        ["xpath=//li[9]/a/span[2]", "xpath:position"]
+      ],
+      "value": ""
+    }]
+  }]
